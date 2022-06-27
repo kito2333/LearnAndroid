35f1bd4dd6cb94da8d68e1243a1b0eddb8ea1421
zhangjinhao <zhangjinhao@bilibili.com>
2022-06-27 18:10:53 +0800

    适配沉浸式状态栏(小米顶部黑边需要使用windowLayoutInDisplayCutoutMode属性适配)

--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import androidx.activity.result.contract.ActivityResultContracts
 import com.example.helloworld.ui.WebViewActivity
 import kotlinx.android.synthetic.main.activity_main.*
--- HelloWorld/app/src/main/java/com/example/helloworld/ui/WebViewActivity.kt
+++ HelloWorld/app/src/main/java/com/example/helloworld/ui/WebViewActivity.kt
@@ -1,21 +1,62 @@
 package com.example.helloworld.ui
 
 import android.annotation.SuppressLint
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import android.webkit.WebViewClient
 import com.example.helloworld.R
 import kotlinx.android.synthetic.main.activity_web_view.*
 
 class WebViewActivity : AppCompatActivity() {
 
-    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web_view)
+        initWebView()
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun initWebView() {
         val url = intent.getStringExtra("url") ?: return
         webView.settings.javaScriptEnabled = true
         webView.webViewClient = WebViewClient()
         webView.loadUrl(url)
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        compatImmersiveMode()
+    }
+
+    // sdk >= 21
+    private fun compatImmersiveMode() {
+        val decorView = window.decorView
+        // 透明状态栏, 隐藏需要用SYSTEM_UI_FLAG_FULLSCREEN
+//        decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+//        decorView.systemUiVisibility =
+//            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//        window.statusBarColor = Color.TRANSPARENT
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            // 适配小米隐藏状态栏后的黑边
+            window.attributes.layoutInDisplayCutoutMode =
+                WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+        }
+
+
+        // 隐藏ActionBar
+        supportActionBar?.hide()
+
+        // 隐藏导航栏和状态栏
+//        decorView.systemUiVisibility =
+//            View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+
+        // 沉浸模式
+        decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+
     }
 }
